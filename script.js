038f9fe8c3d44156e39c776937842df57359820f
kartiknoriya <148875248+kartiknoriya@users.noreply.github.com>
2026-01-01 00:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function playSound(audioId) {
+    const audio = document.getElementById(audioId);
+
+    if (audio) {
+        audio.currentTime = 0; // restart sound
+        audio.play();
+    }
+}
+
+function stopAllSounds() {
+    const allAudios = document.querySelectorAll("audio");
+
+    allAudios.forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
